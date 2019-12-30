126a74f2c77815dc11dceb06e3f24d0d91182a84
Stephanie Lella <stelella2007@gmail.com>
2019-12-30 05:23:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ function logWhisper(string) {console.log(string.toLowerCase())}
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) { return "I can't hear you!"}
 }
-  else if (string.toUpperCase() === string) { return "YES INDEED!"}
+  else (string.toUpperCase() === string) { return "YES INDEED!"}
 }
-else if (string === "I love you, Grandma.") {
-  return "I love you too."
+else (string === "I love you, Grandma.") {
+  return "I love you, too."
 }}
